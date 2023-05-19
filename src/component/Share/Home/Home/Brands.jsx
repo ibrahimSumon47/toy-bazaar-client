4d5692241b335e5b2995f46c00bd467ddc11c9b1
ibrahimSumon47 <ibrahimsumon005@gmail.com>
2023-05-19 06:43:05 +0600

    some brands added

---
+++ src/component/Share/Home/Home/Brands.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Brands = () => {
+    return (
+        <div>
+            <h3>All Brands are here</h3>
+
+            <div className='flex'>
+                <img src="https://i.ibb.co/DGYKg0S/download.png" alt="" />
+                <img src="https://i.ibb.co/BzQ3JQ0/Marvel-Logo-svg.png" alt="" />
+                <img src="https://i.ibb.co/bJzrgvg/2fbc5677b53a19db571ae40f252b749b.webp" alt="" />
+                <img src="https://i.ibb.co/cbWGj54/png-clipart-teenage-mutant-ninja-turtles-logo-teenage-mutant-ninja-turtles-text-logo.png" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Brands;
--- src/component/Share/Home/Home/Home.jsx
+++ src/component/Share/Home/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Main from '../../../Layout/Main';
 import Banner from '../Banner/Banner';
 import Gallery from '../Gallery/Gallery';
+import Brands from './Brands';
 
 const Home = () => {
     return (
         <div className='md:mx-20 my-5'>
             <Banner/>
+            <Brands/>
             <Gallery />
         </div>
     );
